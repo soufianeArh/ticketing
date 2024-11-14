44204f0442f81558dd956f21f876956987859d3d
souf <arhlabi.soufiane02@gmail.com>
2024-11-14 17:19:42 +0800

    express error handler mw

---
+++ auth/src/middlewares/error-handlers.ts
@@ -0,0 +1,8 @@
+import {Request, Response, NextFunction} from 'express';
+export function errorHandler(err: Error, req:Request, res: Response, next:NextFunction){
+      console.log('something went wrong', err )
+
+      res.status(400).send({
+            message:err.message
+      })
+}
