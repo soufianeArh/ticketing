de838dd89d85d4ac63f88e053638370c640ac59e
souf <arhlabi.soufiane02@gmail.com>
2024-11-14 17:18:10 +0800

    middlewares of auth routes+ error middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { currentuserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
 
 const app = express();
 
 app.use(bodyParser.json());
-//tesae
-app.get("/api/users/currentuser", (req, res)=>{
-      res.send("hello via ticketing.dev")
-})
+
+app.use(currentuserRouter)
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
 app.listen(3000, ()=>{
       console.log("express server running at 3000 via ts-node-dev");
       console.log("test21")
