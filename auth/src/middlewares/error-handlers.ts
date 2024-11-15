7bb1978bccb796669cecb3c69a4d7a5ebfd8ffd3
souf <arhlabi.soufiane02@gmail.com>
2024-11-15 17:15:03 +0800

    middleware error logic

--- auth/src/middlewares/error-handlers.ts
+++ auth/src/middlewares/error-handlers.ts
@@ -1,8 +1,19 @@
 import {Request, Response, NextFunction} from 'express';
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
 export function errorHandler(err: Error, req:Request, res: Response, next:NextFunction){
       console.log('something went wrong', err )
-
-      res.status(400).send({
-            message:err.message
-      })
+      if(err instanceof RequestValidationError){
+            //err is []of validationErrors
+            const fromattedErrors = err.errors.map(
+                  error =>{ return {msg : error.msg, filed: error }}
+            )
+             res.status(400).send({errors: fromattedErrors})
+      }
+      if(err instanceof DatabaseConnectionError){
+            res.status(400).send({errors:[{msg: err.reason}]})
+      }else{
+            res.send({errors:"hell"})
+      }
 }
