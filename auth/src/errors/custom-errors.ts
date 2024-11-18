2f03bf58cbe59bcd66f31ece4ba1e4fcb3f8b7b2
souf <arhlabi.soufiane02@gmail.com>
2024-11-18 11:55:32 +0800

    Custom Error class with interface

---
+++ auth/src/errors/custom-errors.ts
@@ -0,0 +1,14 @@
+// interface CustomErrorsA{
+//       statusCode: number,
+//       serializedError():{
+//             message:string,
+//             field?:string
+//       }[]
+// }
+// export class CustomErrors extends Error implements CustomErrorsA{
+      
+//       constructor(statusCode: string){
+//             super()
+//       }
+//       statusCode = this.statusCode
+// }
