bcd9fe4f4fb346029ede6e38782e500f26cc966f
souf <arhlabi.soufiane02@gmail.com>
2024-11-18 11:20:15 +0800

    handler update to match new delegation of instances

--- auth/src/middlewares/error-handlers.ts
+++ auth/src/middlewares/error-handlers.ts
@@ -5,15 +5,12 @@ import { DatabaseConnectionError } from "../errors/database-connection-error";
 export function errorHandler(err: Error, req:Request, res: Response, next:NextFunction){
       console.log('something went wrong', err )
       if(err instanceof RequestValidationError){
-            //err is []of validationErrors
-            const fromattedErrors = err.errors.map(
-                  error =>{ return {msg : error.msg, filed: error }}
-            )
-             res.status(400).send({errors: fromattedErrors})
+            res.status(err.statusCode).send({errors: err.test})
       }
-      if(err instanceof DatabaseConnectionError){
-            res.status(400).send({errors:[{msg: err.reason}]})
-      }else{
+      else if(err instanceof DatabaseConnectionError){
+            res.status(err.statusCode).send({errors: err.serailizeError()})
+      }
+      else{
             res.send({errors:"hell"})
       }
 }
