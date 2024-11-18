256d0f1ba33802d8b252c95a1b1fb4042bd7f7ec
souf <arhlabi.soufiane02@gmail.com>
2024-11-18 14:16:09 +0800

    instance of customeError

--- auth/src/middlewares/error-handlers.ts
+++ auth/src/middlewares/error-handlers.ts
@@ -1,13 +1,11 @@
 import {Request, Response, NextFunction} from 'express';
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from '../errors/custom-error';
 
 export function errorHandler(err: Error, req:Request, res: Response, next:NextFunction){
       console.log('something went wrong', err )
-      if(err instanceof RequestValidationError){
-            res.status(err.statusCode).send({errors: err.serializeError()})
-      }
-      else if(err instanceof DatabaseConnectionError){
+      if(err instanceof CustomError){
             res.status(err.statusCode).send({errors: err.serializeError()})
       }
       else{
