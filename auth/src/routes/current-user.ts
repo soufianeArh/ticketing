527e4f3261fe2947523451f892929a9cc6d028c9
souf <arhlabi.soufiane02@gmail.com>
2024-11-25 16:34:23 +0800

    requireAuth middleware > throw error

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,14 @@
 import express, {Response, Request} from "express";
 
 import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
 router.get(
       "/api/users/currentuser",
       currentUser,
+      requireAuth,
       async ( req: Request , res: Response )=>{
              res.send({currentUser:req.currentUser || null})
 })
