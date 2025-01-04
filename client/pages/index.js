c97ff1b474ec34816165d9f219c1ebcf69a73f67
souf <arhlabi.soufiane02@gmail.com>
2025-01-04 17:37:15 +0800

    refactor getInitalProps f

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,35 +1,20 @@
 import React from 'react';
-import axios from 'axios';
+import buildClient from '../api/client-build';
 
 
-const LandingPage =  (currentUser)=>{
+const LandingPage =  ({currentUser})=>{
       console.log(currentUser)
-          return <h1>You are signed-in </h1>
+          return currentUser?  <h1>You are signed-in </h1>:<h1>you are NOT signe IN</h1>
 }
 
 LandingPage.getInitialProps = async({req})=>{
-     console.log(req.headers.cookie)
-     if(typeof window === 'undefined'){
-          try{
-          const {data}= await axios.get(
-               'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-                    headers:req.headers
-               }
-          );
-          return data;
-         }catch(error){
-          console.log(error);
-          return {email:"none from SSR"}
-         }
-     }else{
-          try{
-               const response = await axios.get('/api/users/currentuser');
-               return {email: response.data}
-              }catch(err){
-               console.log("some error while fetching", err)
-               return {email:"none from browser"}
-              }
-     }
+   try{
+     const {data} = await buildClient(req).get("/api/users/currentuser")
+     return data
+   }catch(err){
+     console.log("browser or ssr error", err)
+     return {currentUser: undefined}
+   }
 }
 
 export default LandingPage
