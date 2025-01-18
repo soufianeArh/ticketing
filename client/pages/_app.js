5b30355847bedb58ca943c93936962891ed1251c
souf <arhlabi.soufiane02@gmail.com>
2025-01-18 14:20:23 +0800

    pass data by calling randomPage getInital in necpage getInit + the return is part of Nextpage param passed to randomPage comp

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,21 +1,29 @@
  import "bootstrap/dist/css/bootstrap.css"
  import buildClient from "../api/client-build"
-const NextPage =  ({Component, pageProps})=>{
+const NextPage =  ({Component, randomPageGetInitilalCall, currentUser})=>{
       return <div>
-            <h1>Header</h1>
-            <Component {...pageProps} />
+            <h1>Header {currentUser.email} </h1>
+            <Component {...randomPageGetInitilalCall} />
       </div> 
 }
 
 
 NextPage.getInitialProps = async(props)=>{
-
-      console.log("geinitial from nectpage", props.ctx.req.headers);
-      // const req = context.req
+      // console.log("geinitial from nectpage", props.ctx.req.headers);
       try{
             const {data} = await buildClient(props.ctx.req).get("/api/users/currentuser")
-            console.log(data)
-            return data
+            let randomPageGetInitilalCall = {};
+            if(props.Component.getInitialProps){
+                  randomPageGetInitilalCall = await props.Component.getInitialProps(props.ctx.req);
+                  console.log("randomPageGetInitilalCall",randomPageGetInitilalCall);//exact retrun of it
+
+            }
+            console.log("nexpage data",data)
+            return {
+                  randomPageGetInitilalCall,
+                  currentUser:data.currentUser,
+                  ...data
+            }
           }catch(err){
             console.log("browser or ssr error", err)
             return {currentUser: undefined}
--- client/pages/index.js
+++ client/pages/index.js
@@ -7,11 +7,11 @@ const LandingPage =  ({currentUser})=>{
           return currentUser?  <h1>You are signed-inmmm </h1>:<h1>you are NOT signe INnnn</h1>
 }
 
-LandingPage.getInitialProps = async({req})=>{
-  console.log("geinitial from randome", req.headers);
-
+LandingPage.getInitialProps = async(req)=>{
+  //console.log("geinitial from randome", props.headers);
    try{
      const {data} = await buildClient(req).get("/api/users/currentuser")
+     console.log("randompage", data)
      return data
    }catch(err){
      console.log("browser or ssr error", err)
