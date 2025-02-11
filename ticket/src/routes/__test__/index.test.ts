7f2ae7f7985966ddbed07191d3ea35694479a8d9
souf <arhlabi.soufiane02@gmail.com>
2025-02-11 10:08:15 +0800

    catch async error and pass them to next to be handled by middleware

--- ticket/src/routes/__test__/index.test.ts
+++ ticket/src/routes/__test__/index.test.ts
@@ -1,10 +1,31 @@
 import request from "supertest";
 import {app} from "../../app";
+import { signin } from "../../test/setup";
 
+function createTicket() {
+      return request(app)
+      .post("/api/tickets")
+      .set("Cookie", signin())
+      .send({
+            title:"coorect title byme",
+            price:10,
+      });
+}
 it("ticket returned all ", async ()=>{
       const response = await request(app)
       .get(`/api/tickets`)
       .send();
 
       expect(response.status).not.toEqual(404);
+});
+it("can fetch a list of tickets", async ()=>{
+      await createTicket();
+      await createTicket();
+      await createTicket();
+
+      const response = await request(app)
+      .get("/api/tickets")
+      .send()
+      .expect(200)
+      expect(response.body.length).toEqual(3)
 })
--- ticket/src/routes/index.ts
+++ ticket/src/routes/index.ts
@@ -1,8 +1,15 @@
 import express , {Request, Response, NextFunction } from "express";
+import { Ticket } from "../models/Ticket";
 
 const router = express.Router();
-router.get("/api/tickets/", (req: Request, res: Response) => {
-      res.send({})
+router.get("/api/tickets", async (req: Request, res: Response, next:NextFunction) => {
+      try{
+            const tickets = await Ticket.find({});
+            res.status(200).send(tickets)
+
+      }catch(err){
+            next(err)
+      }
 })
 
 export {router as getAllTickets}
