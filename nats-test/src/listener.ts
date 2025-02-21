3447f9e4c7b1386541d56f955d8097790f497cbf
souf <arhlabi.soufiane02@gmail.com>
2025-02-21 16:45:43 +0800

    create listen abstract class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, {Message} from "node-nats-streaming";
+import nats, {Message, Stan} from "node-nats-streaming";
 import { randomBytes } from "crypto";
 console.clear()
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"),{
@@ -17,11 +17,13 @@ stan.on("connect",()=>{
      const options = stan
      .subscriptionOptions()
      .setManualAckMode(true)
+     .setDeliverAllAvailable()
+       .setDurableName("accounting-services")
      ;
 
      const subscription = stan.subscribe(
       'ticket:created',
-      'orders-service-queue-group',
+       'orders-service-queue-group',
       options
 )
     
@@ -37,4 +39,47 @@ stan.on("connect",()=>{
 });
 
 process.on('SIGINT', ()=>stan.close());
-process.on('SIGTERM', ()=>stan.close())
+process.on('SIGTERM', ()=>stan.close());
+
+
+abstract class Listener {
+      abstract subject: string;
+      abstract queueGroupName: string
+      abstract onMessage(data:any, msg: Message): void;
+      private client:Stan;
+      protected ackWait = 5 * 1000
+
+      constructor(client:Stan){
+            this.client = client
+            
+      };
+      //a motheod becasue in TS this. is accessed in controcutor or method
+      subscriptionOptions(){
+            return this.client
+            .subscriptionOptions()
+            .setManualAckMode(true)
+            .setAckWait(this.ackWait)
+            .setDeliverAllAvailable()
+            .setDurableName(this.queueGroupName)
+      }
+      listen(){
+            const subscription = this.client.subscribe(
+                  this.subject,
+                  this.queueGroupName,
+                  this.subscriptionOptions()
+            )
+            subscription.on('message', (msg: Message)=>{
+                  console.log(`Message Received: ${this.subject} / ${this.queueGroupName}`)
+                 const parsedData = this.parseMessage(msg)
+                 this.onMessage(parsedData, msg)
+            })
+      }
+      parseMessage(msg:Message){
+            const data = msg.getData()
+            return typeof data === 'string'
+            ? JSON.parse(data)
+            :JSON.parse(data.toString('utf8'))
+      }
+}
+
+
