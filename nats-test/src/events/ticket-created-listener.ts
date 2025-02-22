a8ceb34ab5964652e7c0219bbca75fb95f5d9d17
souf <arhlabi.soufiane02@gmail.com>
2025-02-22 15:35:34 +0800

    saved of provious commit

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,8 +1,10 @@
 import {Listener} from "./base-listener";
 import {Message} from 'node-nats-streaming'
+import { TicketCreateEvent } from "./ticket-created";
+import {Subjects} from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-      subject = "ticket:created"
+export class TicketCreatedListener extends Listener<TicketCreateEvent> {
+      subject: Subjects.TicketCreated = Subjects.TicketCreated
       queueGroupName = "ticket-created-queue"
       onMessage = (data:any, msg: Message)=>{
             msg.ack()
