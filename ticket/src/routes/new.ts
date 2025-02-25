4ec3b5b3030c379199b55b92783df5baca34ac0a
souf <arhlabi.soufiane02@gmail.com>
2025-02-25 15:20:28 +0800

    update route to publish event after save item to mg and before res.send

--- ticket/src/routes/new.ts
+++ ticket/src/routes/new.ts
@@ -3,6 +3,7 @@ import { requireAuth, validateRequest } from '@soufiane12345/ticketing-common';
 import { body } from "express-validator";
 import {Ticket} from "../models/Ticket"
 import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 router.post(
@@ -22,12 +23,12 @@ router.post(
                   userId: req.currentUser!.id
             })
             await ticket.save();
-            // await TicketCreatedPublisher(client).publish({
-            //       id:ticket.id,
-            //       title:ticket.title,
-            //       price:ticket.price,
-            //       userId:ticket.userId
-            // });
+            await new TicketCreatedPublisher(natsWrapper.client).publish({
+                  id:ticket.id,
+                  title:ticket.title,
+                  price:ticket.price,
+                  userId:ticket.userId
+            });
       res.status(201).send(ticket);
 
 });
