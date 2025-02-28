955b17a4dc73af321287d0a49a190de2230d0c24
souf <arhlabi.soufiane02@gmail.com>
2025-02-28 17:28:36 +0800

    newOrder route: request validation

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -2,8 +2,8 @@ import express, {Request, Response} from "express";
 
 const router = express.Router();
 
-router.delete("/api/orders/:id", (req: Request, res: Response) => {
-      
+router.delete("/api/orders/:orderId", (req: Request, res: Response) => {
+      res.send({})
 })
 
 export { router as deleteOrderRouter };
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from "express";
 
 const router = express.Router();
 
-router.get("/api/orders/:id", (req: Request, res: Response) => {
+router.get("/api/orders/:orderId", (req: Request, res: Response) => {
       
 })
 
