89d5da7923b635dfde105c4f4480d5e5953cab80
souf <arhlabi.soufiane02@gmail.com>
2025-03-04 11:13:17 +0800

    index.ts-currentUser gets all his orders setup

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
+import { requireAuth, validateRequest } from "@soufiane12345/ticketing-common";
 import express, {Request, Response} from "express";
+import { Order } from "../Models/order";
 
 const router = express.Router();
 
-router.get("/api/orders", (req: Request, res: Response) => {
-      
+router.get("/api/orders",
+      requireAuth,
+      async (req: Request, res: Response) => {
+      const orders = await Order.find({ userId: req.currentUser!.id})
+      const ordersPopulated =  await Order.find({ userId: req.currentUser!.id})
+      .populate('ticket')
 })
 
 export { router as getAllOrderRouter };
