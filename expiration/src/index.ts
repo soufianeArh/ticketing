d590dc4ca89414df8887760e5cdaa6a3d852388c
souf <arhlabi.soufiane02@gmail.com>
2025-03-13 13:51:50 +0800

    skaffold updated to build and watch expiration + rawYaml of exp redis and depl

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -3,12 +3,7 @@ import { natsWrapper } from "./nats-wrapper";
 
 
 const start = async ()=>{
-      if(!process.env.JWT_KEY){
-            throw new Error('KWT_KEY must be defined in POD process NS!')
-      }
-      if(!process.env.MONGO_URI){
-            throw new Error("DB Uri not added to Environment valirable")
-      }
+   
       if(!process.env.NATS_CLUSTER_ID){
             throw new Error("DB Uri not added to Environment valirable")
       }
