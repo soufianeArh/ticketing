154747749c807b0c752317c03d462474a94b8ac6
souf <arhlabi.soufiane02@gmail.com>
2025-03-21 13:49:47 +0800

    display single tickt in [thiskcet]id + create an order

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,4 +1,34 @@
-const TicketShow = ()=>{
-      return <h1>Ticket Show</h1>
+import useRequest from "../../hooks/use-request"
+
+const TicketShow = ({ticket})=>{
+      const {doRequest, errors} = useRequest({
+            url:"/api/orders",
+            method: "POST", 
+            body:{ticketId:ticket.id},
+            onSuccess:(order)=>{
+                  console.log("order succcess")
+            }
+      })
+
+      return <>
+      <h1>Ticket Show</h1>
+      <h2>{ticket.title}</h2>
+      <h2>{ticket.price}</h2>
+      {errors}
+      <button
+      onClick={doRequest}
+      className="btn btn-primary">
+            purshase
+      </button>
+      </>
+}
+TicketShow.getInitialProps = (context , client) =>{
+      const {ticketId} = context.query
+      try{
+            const {data} = client.get(`/api/tickets/${ticketId}`)
+            return {ticket: data}
+      }catch(err){
+            return {ticketId: undefined}
+      }
 }
 export default TicketShow
